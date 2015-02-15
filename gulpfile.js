702382e0a1ae08f1dd80fd2ee61b115aba224380
Willywongi <pongi@pongi.it>
2015-02-15 21:36:54 +0100

    First commit, with working gulp, babel, livereload setup

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path'),
+	gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    http = require('http'),
+    st = require('st'),
+    babel = require("gulp-babel");
+
+gulp.task('default', ['build', 'server', 'watch']);
+
+gulp.task('build', function() {
+	gulp.src("src/index.html").pipe(gulp.dest("dist"));
+	return gulp.src("src/app.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('src/*', ['build']);
+});
+
+gulp.task('server', function(done) {
+  http.createServer(
+    st({ path: __dirname + '/dist', index: 'index.html', cache: false })
+  ).listen(8080, done);
+});
+
+
---
+++ src/app.js
@@ -0,0 +1,15 @@
+	var App = React.createClass({
+			getInitialState: function() {
+				return {message: ''}
+			},
+			
+			handleChangeMessage: function(e) {
+				this.setState({message: e.target.value});
+			},
+			
+			render: function() {
+				return <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
+			}
+		});
+	React.render(<App />, document.getElementById('content'));
+
