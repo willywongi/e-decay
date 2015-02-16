3aae721e67b163ba6df42ec3f416e459f1828920
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2015-02-16 13:37:47 +0100

    Learning react :)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
-	var App = React.createClass({
-			getInitialState: function() {
-				return {message: ''}
-			},
-			
-			handleChangeMessage: function(e) {
-				this.setState({message: e.target.value});
-			},
-			
-			render: function() {
-				return <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
-			}
-		});
+var Word = React.createClass({
+		render: function() {
+			return (<span>{this.props.text} </span>);
+		} 
+	}),
+	App = React.createClass({
+		getInitialState: function() {
+			return {message: ''}
+		},
+		
+		handleChangeMessage: function(e) {
+			this.setState({message: e.target.value});
+		},
+		
+		render: function() {
+			var words = this.props.words.map(function (word) {
+				return (
+					<Word text={word.text}></Word>
+				);
+			});
+			return (
+				<div>
+					{words}
+				</div>
+			);
+		}
+	});
 $(document).ready(function() {
-	React.render(<App />, document.getElementById('content'));
+	var words = "ciao come stai?".split(" ").map(function(wt, index) {
+		return {'text': wt, key: index};
+	});
+	console.log(words);
+	React.render(<App words={words}/>, document.getElementById('content'));
 });
 	
 
